03ce626cd041d009cb6ca52a4647f67fc72fbb4d
s0ys1516 <klevtsevitch.lev@yandex.ru>
2026-02-03 11:45:13 +0300

    feat:/ fix site title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { HomeView } from '@views/home'
 
 export const metadata: Metadata = {
-  title: 'CenterGroup',
+  title: 'City Group',
   description: ''
 }
 
