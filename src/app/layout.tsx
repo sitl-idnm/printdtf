64dec583c9a087f6c1adc0de4e44f708b8c539b8
s0ys1516 <klevtsevitch.lev@yandex.ru>
2026-02-04 21:33:58 +0300

    feat:/ add counters

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 import { Footer } from '@modules/footer'
 import { Header } from '@modules/header'
+import Script from 'next/script'
 
 import '@styles/global.scss'
 
@@ -61,6 +62,61 @@ export default function RootLayout({
             <div id="modal-root" />
             <ChatWidget />
             <ArrowUp />
+
+            {/* Yandex.Metrika */}
+            <Script id="ym-loader" strategy="afterInteractive">
+              {`
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a);
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+              ym(106625552, 'init', {
+                ssr:true,
+                webvisor:true,
+                clickmap:true,
+                ecommerce:'dataLayer',
+                referrer: document.referrer,
+                url: location.href,
+                accurateTrackBounce:true,
+                trackLinks:true
+              });
+              `}
+            </Script>
+            <noscript>
+              <div>
+                <img src="https://mc.yandex.ru/watch/106625552" style={{position:'absolute', left:'-9999px'}} alt="" />
+              </div>
+            </noscript>
+
+            {/* Top.Mail.Ru */}
+            <Script id="topmail-init" strategy="afterInteractive">
+              {`
+              (function(w) { w._tmr = w._tmr || []; })(window);
+              window._tmr.push({id: "3611462", type: "pageView", start: (new Date()).getTime()});
+              (function (d, w, id) {
+                if (d.getElementById(id)) return;
+                var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+                ts.src = "https://top-fwz1.mail.ru/js/code.js";
+                var f = function () {
+                  var s = d.getElementsByTagName("script")[0];
+                  s.parentNode.insertBefore(ts, s);
+                };
+                if (w.opera == "[object Opera]") {
+                  d.addEventListener("DOMContentLoaded", f, false);
+                } else { f(); }
+              })(document, window, "tmr-code");
+              `}
+            </Script>
+            <noscript>
+              <div>
+                <img src="https://top-fwz1.mail.ru/counter?id=3611462;js=na" style={{position:'absolute', left:'-9999px'}} alt="Top.Mail.Ru" />
+              </div>
+            </noscript>
           </Provider>
         </PageThemeProvider>
       </body>
